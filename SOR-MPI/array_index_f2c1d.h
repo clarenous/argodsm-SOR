8074e551abb9c77374034cc4d17b8abbe43e3922
Ceyu Gao <gaoceyu@gmail.com>
2021-10-26 12:33:29 +0800

    implement sor by Open MPI

--- SOR-MPI/array_index_f2c1d.h
+++ SOR-MPI/array_index_f2c1d.h
@@ -1,40 +1,48 @@
-
 #ifndef __ARRAY_INDEX_F2C_H__
 #define __ARRAY_INDEX_F2C_H__
+
+__attribute__((always_inline)) inline unsigned int F3D2C_kji(
+        unsigned int k_rng, unsigned int j_rng, // ranges, i.e. (hb-lb)+1
+        int k_lb, int j_lb, int i_lb, // lower bounds
+        int kx, int jx, int ix
+) {
+    return (k_rng * j_rng * (ix - i_lb) + k_rng * (jx - j_lb) + kx - k_lb);
+}
+
 __attribute__((always_inline)) inline unsigned int F3D2C(
-        unsigned int i_rng,unsigned int j_rng, // ranges, i.e. (hb-lb)+1
+        unsigned int i_rng, unsigned int j_rng, // ranges, i.e. (hb-lb)+1
         int i_lb, int j_lb, int k_lb, // lower bounds
         int ix, int jx, int kx
-        ) {
-    return (i_rng*j_rng*(kx-k_lb)+i_rng*(jx-j_lb)+ix-i_lb);
+) {
+    return (i_rng * j_rng * (kx - k_lb) + i_rng * (jx - j_lb) + ix - i_lb);
 }
 
 __attribute__((always_inline)) inline unsigned int F2D2C(
         unsigned int i_rng, // ranges, i.e. (hb-lb)+1
         int i_lb, int j_lb, // lower bounds
         int ix, int jx
-        ) {
-    return (i_rng*(jx-j_lb)+ix-i_lb);
+) {
+    return (i_rng * (jx - j_lb) + ix - i_lb);
 }
 
 
 __attribute__((always_inline)) inline unsigned int F1D2C(
         int i_lb, // lower bounds
         int ix
-        ) {
-    return ix-i_lb;
+) {
+    return ix - i_lb;
 }
 
 __attribute__((always_inline)) inline unsigned int F4D2C(
-        unsigned int i_rng,unsigned int j_rng, unsigned int k_rng, // ranges, i.e. (hb-lb)+1
+        unsigned int i_rng, unsigned int j_rng, unsigned int k_rng, // ranges, i.e. (hb-lb)+1
         int i_lb, int j_lb, int k_lb, int l_lb, // lower bounds
         int ix, int jx, int kx, int lx
-        ) {
-    return (i_rng*j_rng*k_rng*(lx-l_lb)+
-            i_rng*j_rng*(kx-k_lb)+
-            i_rng*(jx-j_lb)+
-            ix-i_lb
-            );
+) {
+    return (i_rng * j_rng * k_rng * (lx - l_lb) +
+            i_rng * j_rng * (kx - k_lb) +
+            i_rng * (jx - j_lb) +
+            ix - i_lb
+    );
 }
 
 
--- SOR-MPI/main.cpp
+++ SOR-MPI/main.cpp
@@ -24,19 +24,94 @@ void init_array(float **p0_ptr, float **p1_ptr, float **rhs_ptr, int actual_k_si
     *p1_ptr = new float[actual_3d_size];
     *rhs_ptr = new float[actual_3d_size];
     // initialize
-    for (int k = 1; k < actual_k_size - 1; k += 1) {
-        for (int i = 0; i < im + 2; i += 1) {
-            for (int j = 0; j < jm + 2; j += 1) {
-                (*rhs_ptr)[F3D2C(im + 2, jm + 2, 0, 0, 0, i, j, k)] = 1.0;
-                (*p0_ptr)[F3D2C(im + 2, jm + 2, 0, 0, 0, i, j, k)] = 1.0;
+    for (int k = 0; k < actual_k_size; k += 1) {
+        for (int j = 0; j < jm + 2; j += 1) {
+            for (int i = 0; i < im + 2; i += 1) {
+                (*rhs_ptr)[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)] = 1.0;
+                (*p0_ptr)[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)] = 1.0;
             }
         }
     }
-    printf("Allocated slice, actual_k_size = %d, actual_3d_size = %lld\n", actual_k_size, actual_3d_size);
+    printf("Allocated float slice, actual_k_size = %d, actual_3d_size = %lld\n", actual_k_size, actual_3d_size);
 }
 
-void sor(float *p0, float *p1, float *rhs, int world_rand, int world_size) {
+void find_neighbor_proc(int world_rank, int world_size, int *left, int *right) {
+    if (world_rank > 0) {
+        *left = world_rank - 1;
+    } else {
+        *left = MPI_PROC_NULL;
+    }
+    if (world_rank < world_size - 1) {
+        *right = world_rank + 1;
+    } else {
+        *right = MPI_PROC_NULL;
+    }
+}
 
+void sor(float *p0, float *p1, float *rhs, int world_rank, int world_size, int actual_k_size, int64_t base_2d_size) {
+    // define constants
+    const float cn1 = 1.0 / 3.0;
+    const float cn2l = 0.5;
+    const float cn2s = 0.5;
+    const float cn3l = 0.5;
+    const float cn3s = 0.5;
+    const float cn4l = 0.5;
+    const float cn4s = 0.5;
+    const float omega = 1.0;
+
+    // find neighbor process
+    int down_neighbor, up_neighbor;
+    find_neighbor_proc(world_rank, world_size, &down_neighbor, &up_neighbor);
+//    printf("find neighbor process: world_rank = %d, world_size = %d, down = %d, up = %d\n", world_rank, world_size, down_neighbor, up_neighbor);
+
+    // transfer p0 boundary data
+//    printf("transfer p0 boundary data: world_rank = %d, world_size = %d\n", world_rank, world_size);
+    int tag_up = 1023, tag_down = 1024;
+    MPI_Status status;
+    MPI_Sendrecv(p0 + base_2d_size * (actual_k_size - 2), int(base_2d_size), MPI_FLOAT, up_neighbor, tag_up,
+                 p0, int(base_2d_size), MPI_FLOAT, down_neighbor, tag_up,
+                 MPI_COMM_WORLD, &status);
+    MPI_Sendrecv(p0 + base_2d_size, int(base_2d_size), MPI_FLOAT, down_neighbor, tag_down,
+                 p0 + base_2d_size * (actual_k_size - 1), int(base_2d_size), MPI_FLOAT, up_neighbor, tag_down,
+                 MPI_COMM_WORLD, &status);
+
+    // iterations
+//    printf("computation: world_rank = %d, world_size = %d\n", world_rank, world_size);
+    float rel_tmp;
+    for (int k = 1; k < actual_k_size - 1; k += 1) {
+        for (int j = 0; j < jm + 2; j += 1) {
+            for (int i = 0; i < im + 2; i += 1) {
+                if (i == im + 1) {
+                    p1[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)] =
+                            p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i - im)];
+                } else if (i == 0) {
+                    p1[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)] =
+                            p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i + im)];
+                } else if (j == jm + 1) {
+                    p1[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)] =
+                            p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i - 1)];
+                } else if (j == 0) {
+                    //  We keep the original values
+                    p1[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)] =
+                            p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)];
+                } else if ((world_rank > 0 && world_rank < world_size - 1) || (world_rank == 0 && k > 1) ||
+                           (world_rank == world_size - 1 && k < actual_k_size - 2)) {
+                    //  The actual SOR expression
+                    rel_tmp = omega * (cn1 * (
+                            cn2l * p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i + 1)] +
+                            cn2s * p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i - 1)] +
+                            cn3l * p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j + 1, i)] +
+                            cn3s * p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j - 1, i)] +
+                            cn4l * p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k + 1, j, i)] +
+                            cn4s * p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k - 1, j, i)] -
+                            rhs[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)]) -
+                                       p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)]);
+                    p1[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)] =
+                            p0[F3D2C_kji(actual_k_size, jm + 2, 0, 0, 0, k, j, i)] + rel_tmp;
+                }
+            }
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -56,6 +131,17 @@ int main(int argc, char **argv) {
 
     float *p0, *p1, *rhs = nullptr;
     init_array(&p0, &p1, &rhs, actual_k_size, actual_3d_size);
+
+    const int num_iterations = 5;
+    for (int iter = 1; iter <= num_iterations; iter += 1) {
+        printf("num_iteration: %d / %d (world_rank = %d, world_size = %d)\n", iter, num_iterations,
+               world_rank, world_size);
+        if (iter % 2 == 0) {
+            sor(p1, p0, rhs, world_rank, world_size, actual_k_size, base_2d_size);
+        } else {
+            sor(p0, p1, rhs, world_rank, world_size, actual_k_size, base_2d_size);
+        }
+    }
 
     MPI_Finalize();
 }
