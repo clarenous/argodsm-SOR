801a2993a8eff2ec441a7e816c96e4ad6a005122
Ceyu Gao <gaoceyu@gmail.com>
2021-11-16 11:55:11 +0800

    ux: introduce context

--- SOR-MPI/main.cpp
+++ SOR-MPI/main.cpp
@@ -4,29 +4,13 @@
 #include "sor_params.h"
 #include <unistd.h>
 
-void divide_params(int world_rank, int world_size, int *base_k_size, int *actual_k_size) {
+void divide_params(int world_rank, int world_size, int *base_k_size, int *base_k_rem, int *actual_k_size) {
     *base_k_size = (km + 2) / world_size;
-    int k_rem = (km + 2) % world_size;
+    *base_k_rem = (km + 2) % world_size;
     if (world_rank == 0) {
-        *base_k_size += k_rem;
+        *base_k_size += *base_k_rem;
     }
     *actual_k_size = *base_k_size + 2;
-}
-
-void divide_location(int world_size, int k, int *target_world_rank, int *target_k) {
-    if (world_size == 1) {
-        *target_world_rank = 0;
-        *target_k = k;
-        return;
-    }
-    int base_k_size = (km + 2) / world_size;
-    *target_world_rank = k / base_k_size;
-    if (*target_world_rank == 0) {
-        *target_k = k;
-    } else {
-        int k_rem = (km + 2) % world_size;
-        *target_k = k - (*target_world_rank - 1) * base_k_size - (base_k_size + k_rem);
-    }
 }
 
 void size_params(int base_k_size, int actual_k_size,
@@ -36,7 +20,27 @@ void size_params(int base_k_size, int actual_k_size,
     *actual_3d_size = *base_2d_size * (int64_t) actual_k_size;
 }
 
-void init_array(float **p0_ptr, float **p1_ptr, float **rhs_ptr, int actual_k_size, int64_t actual_3d_size) {
+struct Context {
+    int world_size;
+    int world_rank;
+    int base_k_size;
+    int base_k_rem;
+    int actual_k_size;
+    int64_t base_2d_size;
+    int64_t base_3d_size;
+    int64_t actual_3d_size;
+};
+
+void init_context(Context *ctx) {
+    MPI_Comm_size(MPI_COMM_WORLD, &ctx->world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ctx->world_rank);
+    divide_params(ctx->world_rank, ctx->world_size, &ctx->base_k_size, &ctx->base_k_rem, &ctx->actual_k_size);
+    size_params(ctx->base_k_size, ctx->actual_k_size, &ctx->base_2d_size, &ctx->base_3d_size, &ctx->actual_3d_size);
+}
+
+void init_array(Context *ctx, float **p0_ptr, float **p1_ptr, float **rhs_ptr) {
+    int actual_k_size = ctx->actual_k_size;
+    int64_t actual_3d_size = ctx->actual_3d_size;
     *p0_ptr = new float[actual_3d_size];
     *p1_ptr = new float[actual_3d_size];
     *rhs_ptr = new float[actual_3d_size];
@@ -66,7 +70,13 @@ void find_neighbor_proc(int world_rank, int world_size, int *left, int *right) {
     }
 }
 
-void sor(float *p0, float *p1, float *rhs, int world_rank, int world_size, int actual_k_size, int64_t base_2d_size) {
+void sor(Context *ctx, float *p0, float *p1, float *rhs) {
+    // define variables
+    int world_rank = ctx->world_rank;
+    int world_size = ctx->world_size;
+    int actual_k_size = ctx->actual_k_size;
+    int64_t base_2d_size = ctx->base_2d_size;
+
     // define constants
     const float cn1 = 1.0 / 3.0;
     const float cn2l = 0.5;
@@ -80,10 +90,8 @@ void sor(float *p0, float *p1, float *rhs, int world_rank, int world_size, int a
     // find neighbor process
     int down_neighbor, up_neighbor;
     find_neighbor_proc(world_rank, world_size, &down_neighbor, &up_neighbor);
-//    printf("find neighbor process: world_rank = %d, world_size = %d, down = %d, up = %d\n", world_rank, world_size, down_neighbor, up_neighbor);
 
     // transfer p0 boundary data
-//    printf("transfer p0 boundary data: world_rank = %d, world_size = %d\n", world_rank, world_size);
     int tag_up = 1023, tag_down = 1024;
     MPI_Status status;
     MPI_Sendrecv(p0 + base_2d_size * (actual_k_size - 2), int(base_2d_size), MPI_FLOAT, up_neighbor, tag_up,
@@ -93,8 +101,7 @@ void sor(float *p0, float *p1, float *rhs, int world_rank, int world_size, int a
                  p0 + base_2d_size * (actual_k_size - 1), int(base_2d_size), MPI_FLOAT, up_neighbor, tag_down,
                  MPI_COMM_WORLD, &status);
 
-    // iterations
-//    printf("computation: world_rank = %d, world_size = %d\n", world_rank, world_size);
+    // computation
     float rel_tmp;
     for (int i = 0; i < im + 2; i += 1) {
         for (int j = 0; j < jm + 2; j += 1) {
@@ -133,10 +140,19 @@ void sor(float *p0, float *p1, float *rhs, int world_rank, int world_size, int a
     }
 }
 
-void print_number_from_array(float *arr, int world_rank, int world_size, int i, int j, int k) {
+void divide_location(Context *ctx, int k, int *target_world_rank, int *target_k) {
+    k = k - ctx->base_k_rem;
+    *target_world_rank = k / ctx->base_k_size;
+    *target_k = k - (*target_world_rank) * (ctx->base_k_size);
+    if (*target_world_rank == 0) {
+        *target_k += ctx->base_k_rem;
+    }
+}
+
+void print_number_from_array(Context *ctx, float *arr, int i, int j, int k) {
     int target_world_rank, target_k;
-    divide_location(world_size, k, &target_world_rank, &target_k);
-    if (world_rank == target_world_rank) {
+    divide_location(ctx, k, &target_world_rank, &target_k);
+    if (ctx->world_rank == target_world_rank) {
         std::cout << "" << arr[F3D2C(im + 2, jm + 2, 0, 0, 0, i, j, target_k + 1)] << "\n";
     }
 }
@@ -158,35 +174,26 @@ int main(int argc, char **argv) {
 
     clock_t total_start = clock();
 
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    int base_k_size, actual_k_size;
-    divide_params(world_rank, world_size, &base_k_size, &actual_k_size);
-
-    int64_t base_2d_size, base_3d_size, actual_3d_size;
-    size_params(base_k_size, actual_k_size, &base_2d_size, &base_3d_size, &actual_3d_size);
+    Context ctx{};
+    init_context(&ctx);
 
     float *p0, *p1, *rhs = nullptr;
-    init_array(&p0, &p1, &rhs, actual_k_size, actual_3d_size);
+    init_array(&ctx, &p0, &p1, &rhs);
 
     const int num_iterations = 5;
     for (int iter = 1; iter <= num_iterations; iter += 1) {
         printf("num_iteration: %d / %d (world_rank = %d, world_size = %d)\n", iter, num_iterations,
-               world_rank, world_size);
+               ctx.world_rank, ctx.world_size);
         if (iter % 2 == 0) {
-            sor(p1, p0, rhs, world_rank, world_size, actual_k_size, base_2d_size);
+            sor(&ctx, p1, p0, rhs);
         } else {
-            sor(p0, p1, rhs, world_rank, world_size, actual_k_size, base_2d_size);
+            sor(&ctx, p0, p1, rhs);
         }
     }
 
     clock_t total_end = clock();
     double total_time = (double) (total_end - total_start) / CLOCKS_PER_SEC;
-    print_number_from_array(p0, world_rank, world_size, im / 2, jm / 2, km / 2);
+    print_number_from_array(&ctx, p0, im / 2, jm / 2, km / 2);
     std::cout << "Total: " << total_time << std::endl;
 //    sleep(world_rank+2);
 //    print_array(p0, actual_k_size);
